a1b2596f8aacd51d6006cbd1a5ab1f0b9ac71e37
Rojan Gh <rojan.gh@noordigital.com>
2023-01-06 14:45:31 +0100

    More similar to official tool

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,90 @@
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 5105:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.NoFileOptions = exports.Inputs = void 0;
+/* eslint-disable no-unused-vars */
+var Inputs;
+(function (Inputs) {
+    Inputs["Name"] = "name";
+    Inputs["Path"] = "path";
+    Inputs["IfNoFilesFound"] = "if-no-files-found";
+})(Inputs = exports.Inputs || (exports.Inputs = {}));
+var NoFileOptions;
+(function (NoFileOptions) {
+    /**
+     * Default. Output a warning but do not fail the action
+     */
+    NoFileOptions["warn"] = "warn";
+    /**
+     * Fail the action with an error message
+     */
+    NoFileOptions["error"] = "error";
+    /**
+     * Do not output any warnings or errors, the action does not fail
+     */
+    NoFileOptions["ignore"] = "ignore";
+})(NoFileOptions = exports.NoFileOptions || (exports.NoFileOptions = {}));
+//# sourceMappingURL=constants.js.map
+
+/***/ }),
+
+/***/ 2202:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.getInputs = void 0;
+const core = __importStar(__nccwpck_require__(2186));
+const constants_1 = __nccwpck_require__(5105);
+/**
+ * Helper to get all the inputs for the action
+ */
+function getInputs() {
+    const name = core.getInput(constants_1.Inputs.Name);
+    const path = core.getInput(constants_1.Inputs.Path, { required: true });
+    const ifNoFilesFound = core.getInput(constants_1.Inputs.IfNoFilesFound);
+    const noFileBehavior = constants_1.NoFileOptions[ifNoFilesFound];
+    if (!noFileBehavior) {
+        core.setFailed(`Unrecognized ${constants_1.Inputs.IfNoFilesFound} input. Provided: ${ifNoFilesFound}. Available options: ${Object.keys(constants_1.NoFileOptions)}`);
+    }
+    const inputs = {
+        artifactName: name,
+        searchPath: path,
+        ifNoFilesFound: noFileBehavior
+    };
+    return inputs;
+}
+exports.getInputs = getInputs;
+//# sourceMappingURL=input-helper.js.map
+
+/***/ }),
+
 /***/ 3109:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -47,18 +131,34 @@ const fs = __importStar(__nccwpck_require__(5747));
 const crypto = __importStar(__nccwpck_require__(6417));
 const path_1 = __nccwpck_require__(5622);
 const search_1 = __nccwpck_require__(2506);
+const constants_1 = __nccwpck_require__(5105);
+const input_helper_1 = __nccwpck_require__(2202);
 function run() {
     var _a, _b, _c;
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const name = core.getInput('name');
-            const path = core.getInput('path');
+            const inputs = (0, input_helper_1.getInputs)();
             const key = core.getInput('key');
             const id = core.getInput('id');
             const bucket = core.getInput('bucket');
-            const searchResult = yield (0, search_1.findFilesToUpload)(path);
-            if (searchResult.filesToUpload.length === 0)
-                throw new Error('No files to upload');
+            const searchResult = yield (0, search_1.findFilesToUpload)(inputs.searchPath);
+            if (searchResult.filesToUpload.length === 0) {
+                // No files were found, different use cases warrant different types of behavior if nothing is found
+                switch (inputs.ifNoFilesFound) {
+                    case constants_1.NoFileOptions.warn: {
+                        core.warning(`No files were found with the provided path: ${inputs.searchPath}. No artifacts will be uploaded.`);
+                        return;
+                    }
+                    case constants_1.NoFileOptions.error: {
+                        core.setFailed(`No files were found with the provided path: ${inputs.searchPath}. No artifacts will be uploaded.`);
+                        return;
+                    }
+                    case constants_1.NoFileOptions.ignore: {
+                        core.info(`No files were found with the provided path: ${inputs.searchPath}. No artifacts will be uploaded.`);
+                        return;
+                    }
+                }
+            }
             core.info(`Root directory: ${searchResult.rootDirectory}`);
             core.info(`${searchResult.filesToUpload.length} files to upload`);
             core.startGroup('Files details');
